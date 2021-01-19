c3da680c7d4e935f1c95cb3c3af26a519cf68b70
KangDaeHan <smrkdeogks@gmail.com>
2021-01-19 16:44:09 +0900

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `pedantic-goldstine-0ef7ce.netlify.app`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://devblogstory.herokuapp.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
